d718027aa45cf1161c881bc67df0494f1ea0b8bc
Nischay <barejanischay5@gmail.com>
2025-07-09 11:41:20 +0530

    workflow making in progress

--- controllers/subscription.contoller.js
+++ controllers/subscription.contoller.js
@@ -1,5 +1,7 @@
+import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js";
 import User from "../models/user.model.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
     try {
@@ -8,10 +10,27 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id,
 
         });
+
+        const workflowResponse = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription._id,
+
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0
+        })
+
         res.status(201).json({
             success: true,
-            data: subscription
+            data: {
+                subscription,
+                workflowId: workflowResponse.workflowRunId
+            }
         });
+
     }
     catch (error) {
         next(error);
@@ -25,8 +44,6 @@ export const getUserSubscriptions = async (req, res, next) => {
             const error = new Error("you are not he owner of this account");
             error.statusCode = 401;
             throw error;
-
-
         }
 
         const subscriptions = await Subscription.find({ user: req.params.id });
